456445a2b24266a906fbd1e2f0f1026b4dd47ef6
moonjukhim <moonju.khim@gmail.com>
2025-07-08 13:31:24 +0900

    Update cool_classes.py

--- src/reflexion/cool_classes.py
+++ src/reflexion/cool_classes.py
@@ -4,17 +4,17 @@ from pydantic import BaseModel, Field
 
 
 class Reflection(BaseModel):
-    missing: str = Field(description="Critique of what is missing.")
-    superfluous: str = Field(description="Critique of what is superfluous")
+    missing: str = Field(description="누락된 부분에 대한 비판")
+    superfluous: str = Field(description="불필요한 것에 대한 비판")
 
 
 class AnswerQuestion(BaseModel):
     """Answer the question."""
 
-    answer: str = Field(description="~250 word detailed answer to the question.")
-    reflection: Reflection = Field(description="Your reflection on the initial answer.")
+    answer: str = Field(description="질문에 대한 답변은 250자 내로 작성")
+    reflection: Reflection = Field(description="첫 번째 답변에 대한 평가")
     search_queries: List[str] = Field(
-        description="1-3 search queries for researching improvements to address the critique of your current answer."
+        description="현재 답변에 대한 비판을 해결하기 위한 개선 사항을 조사하기 위한 1~3개의 검색어"
     )
 
 
@@ -23,5 +23,5 @@ class ReviseAnswer(AnswerQuestion):
     """Revise your original answer to your question."""
 
     references: List[str] = Field(
-        description="Citations motivating your updated answer."
+        description="업데이트된 답변에 대한 동기를 인용"
     )
